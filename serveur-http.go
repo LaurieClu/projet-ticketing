f85205461f4ec9d2b0e69dfb72f23b278e3fb698
laurie cluzeau <lau.cluzeau@gmail.com>
2020-03-18 20:32:07 +0100

    Affichage du premier champ du formulaire dans le serveur http

--- serveur-http.go
+++ serveur-http.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-    http.HandleFunc("/", HelloServer)
+    http.HandleFunc("/", Inscription)
     http.ListenAndServe(":80", nil)
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+func Inscription(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Inscription, %s!", r.URL.Path[1:])
 }
