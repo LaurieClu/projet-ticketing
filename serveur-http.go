92a0cbb292573e7464858fbe1fab542781c5a56e
laurie cluzeau <lau.cluzeau@gmail.com>
2020-03-30 13:55:14 +0200

    tests pour rediriger mon index.html dans mon serveur http

--- serveur-http.go
+++ serveur-http.go
@@ -5,14 +5,18 @@ import (
 	"net/http"
 )
 
-func HttpFileHandler(response http.ResponseWriter, request *http.Request) {
-	http.ServeFile(response, request, "index.html")
+func serveFiles(w http.ResponseWriter, r *http.Request) {
+	p := "." + (r.URL.Path)
+	if p == "./" {
+		p = "./static.index.html"
+	}
+	http.ServeFile(w, r, p)
 }
 func main() {
 	http.HandleFunc("/", Inscription)
 	fmt.Println("Server Starting")
 	http.ListenAndServe(":80", nil)
-	http.HandleFunc("/index.html", HttpFileHandler)
+	http.HandleFunc("/index.html", serveFiles)
 }
 
 func Inscription(w http.ResponseWriter, r *http.Request) {
