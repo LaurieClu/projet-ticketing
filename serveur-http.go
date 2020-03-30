05029c7d235c1f6347e376724d037e9d6942fa41
laurie cluzeau <lau.cluzeau@gmail.com>
2020-03-30 14:29:24 +0200

    tests template

--- serveur-http.go
+++ serveur-http.go
@@ -2,21 +2,28 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func serveFiles(w http.ResponseWriter, r *http.Request) {
-	p := "." + (r.URL.Path)
-	if p == "./" {
-		p = "./static.index.html"
-	}
-	http.ServeFile(w, r, p)
+type Nouveaux struct {
+	Id   int
+	Name string
 }
+
+func indexHTMLTemplateVariableHandler(w http.ResponseWriter, r *http.Request) {
+	tmplt := template.New("index-templated.html")
+	p := Nouveaux{Id: 1, Name: "chavdif"}
+
+	tmplt.Execute(w, p)
+
+}
+
 func main() {
 	http.HandleFunc("/", Inscription)
 	fmt.Println("Server Starting")
 	http.ListenAndServe(":80", nil)
-	http.HandleFunc("/index.html", serveFiles)
+	http.HandleFunc("/index-templated.html", indexHTMLTemplateVariableHandler)
 }
 
 func Inscription(w http.ResponseWriter, r *http.Request) {
