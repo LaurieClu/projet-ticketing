bcbe9d8e2676f8d6164e33ab75e1ca55dc2772df
laurie cluzeau <lau.cluzeau@gmail.com>
2020-03-31 12:59:28 +0200

    ajout du form html

--- serveur-http.go
+++ serveur-http.go
@@ -2,38 +2,22 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
-	"io"
 )
 
-type Nouveaux struct {
-	Id   int
-	Name string
-}
-
-func indexHTMLTemplateVariableHandler(w http.ResponseWriter, r *http.Request) {
-	tmplt := template.New("index-templated.html")
-	p := Nouveaux{Id: 1, Name: "chavdif"}
-
-	tmplt.Execute(w, p)
-}
-
 func main() {
-	http.HandleFunc("/", Inscription)
+
 	fmt.Println("Server Starting")
+	//http.HandleFunc("/", Inscription)
+	http.HandleFunc("/index", serveFiles)
 	http.ListenAndServe(":80", nil)
-	http.HandleFunc("/index-templated.html", indexHTMLTemplateVariableHandler)
 }
 
-func Inscription(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Inscription, %s!", r.URL.Path[1:])
-	v := req.FormValue("q")
-w.Header().Set("Content-Type", "text/html; charset=utf-8")
-io.WriteString(w,
-<form method="POST">
-<input type="text" name="q">
-<input type="envoyer">
-</form>
-<br>`+v)
+func serveFiles(response http.ResponseWriter, request *http.Request) {
+	http.ServeFile(response, request, "index.html")
 }
+
+//func serveFile(w http.ResponseWriter, r *http.Request) {
+//	http.ServeFile(w, r, p)
+
+//func Inscription(w http.ResponseWriter, r *http.Request) {
