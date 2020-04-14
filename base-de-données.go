65c8f5d264e6a8aa93a28d02b1a216f8fc7e8108
laurie cluzeau <lau.cluzeau@gmail.com>
2020-04-14 18:37:23 +0200

    ajout de la request

--- base-de-données.go
+++ base-de-données.go
@@ -2,15 +2,45 @@ package main
 
 import (
 	"database/sql"
-	 _ "github.com/go-sql-driver/mysql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	db, err := sql.Open("mysql",
-		"user:password@tcp(127.0.0.1:3306)/users_ticketing") => DSN
+		"newproject:12345@tcp(localhost)/publications")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var (
+		id   int
+		name string
+	)
+	rows, err := db.Query("select id, name from users_ticketing where id = ?", 2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(id, name)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
