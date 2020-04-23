5ffd4bc283476bf5aec50b092cca79cca7503bf4
laurie cluzeau <lau.cluzeau@gmail.com>
2020-04-23 11:24:02 +0200

    clean code

--- base-de-données.go
+++ base-de-données.go
@@ -7,38 +7,23 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
+var (
+	id   int
+	name string
+)
+
+func createDB() (db *sql.DB) {
 	db, err := sql.Open("mysql",
 		"newproject:12345@tcp(localhost)/publications")
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer db.Close()
-
-	var (
-		id   int
-		name string
-	)
-	rows, err := db.Query("select id, name from users_ticketing where id = ?", 2)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer db.Close()
-
-	for rows.Next() {
-		err := rows.Scan(&id, &name)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer db.Close()
-	}
-
-	newusers(db, "riendutout")
+	return db
 }
 
-func newusers(db *sql.DB, name string) {
+func newUsers(name string) {
+	db := createDB()
 	tsql := fmt.Sprintf("INSERT INTO users_ticketing(name) values('%s');",
 		name)
 	_, err := db.Exec(tsql)
@@ -48,4 +33,24 @@ func newusers(db *sql.DB, name string) {
 
 	}
 	return
+}
+
+// printAllIDAllName parcour et affiche tous les ID & name de la BDD
+func printAllIDAllName() {
+	db := createDB()
+	rows, err := db.Query("select id, name from users_ticketing")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer db.Close()
+		fmt.Println(id, name)
+	}
 }
